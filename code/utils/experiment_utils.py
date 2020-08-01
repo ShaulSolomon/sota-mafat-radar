4e3b828d6c56a3a7998c5bf6a588b231aa6d081f
Hezi H <hershkoy@gmail.com>
2020-08-02 00:51:40 +0300

    add plot confusion matrix, add idx as output of split_train_val

--- code/utils/experiment_utils.py
+++ code/utils/experiment_utils.py
@@ -7,7 +7,8 @@ import os
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, roc_curve, auc
+from sklearn.metrics import confusion_matrix
+import itertools
 
     
 spectrogram_cmap = np.array([[2.422e-01, 1.504e-01, 6.603e-01],
@@ -544,7 +545,7 @@ def fft(iq, axis=0):
   iq = np.log(np.abs(np.fft.fft(hann(iq), axis=axis)))
   return iq
 
-  
+
 def max_value_on_doppler(iq, doppler_burst):
   """
   Set max value on I/Q matrix using doppler burst vector. 
@@ -625,7 +626,7 @@ def split_train_val(data):
   training_y = data['target_type'][np.logical_not(idx)]
   validation_x = data['iq_sweep_burst'][idx]
   validation_y = data['target_type'][idx]
-  return training_x, training_y, validation_x, validation_y
+  return training_x, training_y, validation_x, validation_y, idx
 
 def aux_split(data):
   """
@@ -674,3 +675,37 @@ def stats(pred, actual):
   plt.title('Training set vs. Validation set ROC curves')
   plt.legend(loc="lower right", prop = {'size': 20})
   plt.show()
+  
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
