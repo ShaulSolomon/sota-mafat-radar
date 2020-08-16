2bd00201fec17a19be9e7b535019eb7fcb684265
Shaul Solomon <shaulsolomon@gmail.com>
2020-08-16 19:07:58 +0300

    Working but weak Adam

--- notebooks/model_arch/alex_model.py
+++ notebooks/model_arch/alex_model.py
@@ -7,19 +7,19 @@ class AlexNet2(nn.Module):
 
     def __init__(self):
         super(AlexNet2, self).__init__()
-        features = list(alexnet(pretrained=True).features)
-        self.features = nn.ModuleList(features).eval()
-        self.features.classifier[-1] = nn.Linear(4096,1)
+        self.layers = alexnet(pretrained=True)
+        self.layers.classifier[-1] = nn.Linear(4096,1)
+        for i,layers in enumerate(self.layers.features):
+            if "MaxPool2d" in str(layers):
+                self.layers.features[i] = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
 
     def forward(self,x):
         x = x.permute(3,1,2,0)
         x = x.repeat(3,1,1,1)
-        x = x.permute(3,1,2,0)
-        x = x.permute(0,3,1,2)
-
- 
-
-def classic_alex():
-    model = torchvision.models.alexnet(pretrained=True)
-    model.classifier[6] = nn.Linear(4096,1) # change the last layer to have only two classes
-    return model
+        x = x.permute(3,0,1,2)
+        for feat in self.layers.features:
+            x = feat(x)
+        x = self.layers.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.layers.classifier(x)
+        return F.sigmoid(x)
