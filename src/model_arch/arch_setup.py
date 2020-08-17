fixed issues with auc and loss. accuracy still seems to give wrong values

src/model_arch/arch_setup.py
@@ -1,7 +1,7 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 import numpy as np
-from sklearn.metrics import roc_auc_score, roc_curve, auc, accuracy_score
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 
 
 
@@ -21,11 +21,19 @@ class DS(Dataset):
 
 def pretty_log(log):
     for key,value in log.items():
-        print("{} : {:.4f}".format(key,value))
+        value_s = value if type(value)=="int" else "{:.4f}".format(value)
+        print(f"{key} : {value_s}, ",end="")
     print("\n---------------------------\n")
 
 def thresh(output, thresh_hold = 0.5):
     return [0 if x <thresh_hold else 1 for x in output]
+
+
+def accuracy_calc(outputs, labels):
+    #print("acc1:",outputs, labels)
+    preds = thresh(outputs)
+    #print("acc2:",preds)
+    return np.sum(preds == labels) / len(preds)
 
 def train_epochs(tr_loader,val_loader,model,criterion,optimizer, num_epochs, train_y,val_y):
 
@@ -45,14 +53,20 @@ def train_epochs(tr_loader,val_loader,model,criterion,optimizer, num_epochs, tra
         tr_loss = 0
         tr_size = 0
         tr_y_hat = np.array([])
+        tr_labels = np.array([])
 
+        #train loop
         for step,batch in enumerate(tr_loader):
 
             data, labels = batch
+            tr_labels = np.append(tr_labels,labels)
 
             data = data.to(device,dtype=torch.float32)
             labels = labels.to(device,dtype=torch.float32)
             outputs = model(data)
+
+            labels = labels.view(-1,1)
+            outputs = outputs.view(-1,1)
 
             loss = criterion(outputs,labels)
             loss.backward()
@@ -60,7 +74,7 @@ def train_epochs(tr_loader,val_loader,model,criterion,optimizer, num_epochs, tra
             tr_loss+=loss.item()
             tr_size+=data.shape[0]
 
-            tr_y_hat = np.append(tr_y_hat,thresh(outputs.detach().cpu().numpy()))
+            tr_y_hat = np.append(tr_y_hat,outputs.detach().cpu().numpy())
 
             optimizer.step()
             optimizer.zero_grad()
@@ -68,36 +82,47 @@ def train_epochs(tr_loader,val_loader,model,criterion,optimizer, num_epochs, tra
         val_loss = 0
         val_size = 0
         val_y_hat = np.array([])
+        val_labels = np.array([])
 
+        #validation loop
         for step, batch in enumerate(val_loader):
 
             data, labels = batch
+            val_labels = np.append(val_labels,labels)
+
             data = data.to(device,dtype=torch.float32)
             labels = labels.to(device,dtype=torch.float32)
             outputs = model(data)
+
+            labels = labels.view(-1,1)
+            outputs = outputs.view(-1,1)
 
             loss = criterion(outputs,labels)
 
             val_loss += loss.item()
             val_size += data.shape[0]
 
-            val_y_hat = np.append(val_y_hat,thresh(outputs.detach().cpu().numpy()))
+            val_y_hat = np.append(val_y_hat,outputs.detach().cpu().numpy())
 
 
-        tr_fpr, tr_tpr, _ = roc_curve(train_y, tr_y_hat)
-        val_fpr, val_tpr, _ = roc_curve(val_y, val_y_hat)
+        tr_fpr, tr_tpr, _ = roc_curve(tr_labels, tr_y_hat)
+
+        temp = accuracy_calc(tr_labels,tr_y_hat)
+        print("temp:",temp)
+
+        val_fpr, val_tpr, _ = roc_curve(val_labels, val_y_hat)
 
         epoch_log = {'epoch': epoch,
-                     'loss': tr_loss / tr_size,
+                     'loss': tr_loss ,
                      'auc': auc(tr_fpr, tr_tpr),
-                     'acc': accuracy_score(train_y,tr_y_hat),
-                     'val_loss': val_loss / val_size,
+                     'acc': accuracy_calc(tr_y_hat,train_y),
+                     'val_loss': val_loss ,
                      'val_auc': auc(val_fpr,val_tpr),
-                     'val_acc': accuracy_score(val_y,val_y_hat)}
+                     'val_acc': accuracy_calc(val_y_hat,val_y)}
 
 
         pretty_log(epoch_log)
 
         training_log.append(epoch_log)
 
-    return training_log
+    #return training_log

src/model_arch/base_base_model.py
@@ -28,4 +28,4 @@ class base_base_model(nn.Module):
         x = torch.flatten(x).reshape(batch_size,-1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return F.sigmoid(self.fc3(x))
+        return torch.sigmoid(self.fc3(x))