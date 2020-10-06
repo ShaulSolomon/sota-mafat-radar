f0958f6bccf618b726e17fb67abe1efca21c1677
Adam Cohn <38065591+adcoh@users.noreply.github.com>
2020-10-06 14:11:22 +0300

    Save debug progress and TODOs

--- src/data/iterable_dataset.py
+++ src/data/iterable_dataset.py
@@ -331,17 +331,17 @@ class DataDict(object):
         df['usable'] = np.select(conditions, conditions, default=False)
         df.loc[df['is_validation'], 'usable'] = False
         df.loc[df['is_validation'].shift(1).bfill(), 'usable'] = False
-        # df['subtrack_id'] = df.groupby('track_id')['segment_id'].transform('rank')
-        # df.groupby((df['usable'].shift() != df['usable']).cumsum())
-
         # save validation segments to object and drop from current DF
         val_df = df.loc[df.is_validation].copy().set_index('segment_id')
         df = df.loc[~df.is_validation].copy()
-
+        # TODO find a better way to create subtracks already here - perhaps using a smart groupby transform to group only consecutive segment ids
+        # df['subtrack_id'] = df.groupby('track_id')['segment_id'].transform('rank')
+        # df.groupby((df['usable'].shift() != df['usable']).cumsum())
         df_tracks = df.groupby('track_id').agg(target_type=pd.NamedAgg(column="target_type", aggfunc=list),
                                                usable=pd.NamedAgg(column="usable", aggfunc=list),
                                                output_array=pd.NamedAgg(column="iq_sweep_burst", aggfunc=list),
                                                doppler_burst=pd.NamedAgg(column="doppler_burst", aggfunc=list),
+                                               segment_id=pd.NamedAgg(column="segment_id", aggfunc=list),
                                                )
         df_tracks['output_array'] = df_tracks['output_array'].apply(lambda x: np.concatenate(x, axis=-1))
 
@@ -361,6 +361,7 @@ class DataDict(object):
         df_tracks['doppler_burst'] = df_tracks['doppler_burst'].apply(lambda x: np.concatenate(x, axis=-1))
         df_tracks['target_type'] = df_tracks['target_type'].apply(np.array)
         df_tracks['usable'] = df_tracks['usable'].apply(np.array)
+        df_tracks['segment_id'] = df_tracks['segment_id'].apply(np.array)
         return df_tracks.to_dict(orient='index'), val_df.to_dict(orient='index')
 
 
@@ -378,6 +379,7 @@ def filter_usable_segments(data, output_data_type) -> List[_Segment]:
     burst = data['doppler_burst']
     previous_i = 0
     tracks = []
+    # TODO remove this, the splitting algorithm isn't working as it should after removing the validation segments in line 336, replace with operation on dataframe to create subtrack/segment IDs already at that stage
     for i, use in enumerate(data['usable']):
         if not use:
             if data['usable'][previous_i]:
@@ -396,7 +398,7 @@ def filter_usable_segments(data, output_data_type) -> List[_Segment]:
             previous_i = i + 1
     if not tracks:
         if track.shape[1] == 32:
-            tracks.append(_Segment(segment_id=data['segment_id'],
+            tracks.append(_Segment(segment_id=data['segment_id'][0],
                                    output_array=track,
                                    doppler_burst=burst,
                                    target_type=data['target_type']))
@@ -430,7 +432,7 @@ class StreamingDataset(IterableDataset):
         self.is_val = is_val
         random.shuffle(self.random_index)
 
-    def segments_generator(self, data_dict) -> DataLoader:
+    def segments_generator(self, data_dict) -> List[_Segment]:
         """
         Generates new and/or augmented segments according to configuration parameters.
         Returns a dictionary containing the merged set of segments indexed by
@@ -460,10 +462,12 @@ class StreamingDataset(IterableDataset):
             flips = create_flipped_segments(resplit_data, flip_type='horizontal')
             resplit_data = resplit_data + flips
 
-        return DataLoader(TrackDS(resplit_data), batch_size=1, shuffle=True)
+        # return DataLoader(resplit_data, batch_size=1, shuffle=True)
+        return resplit_data
 
     def get_segment_stream(self):
-        return chain.from_iterable(map(self.segments_generator, self.data))
+
+        return chain.from_iterable(map(self.segments_generator, self.data.values()))
 
     def get_segment_streams(self):
         return zip(*[self.get_segment_stream() for _ in range(self.config['batch_size'])])
@@ -480,7 +484,7 @@ class StreamingDataset(IterableDataset):
         return [cls(dataset=data_list, config=config) for _ in range(num_workers)]
 
     def __iter__(self):
-        return self.get_segment_streams()
+        return self.get_segment_stream()
 
 
 class MultiStreamDataLoader:
@@ -488,7 +492,7 @@ class MultiStreamDataLoader:
         self.datasets = datasets
 
     def get_stream_loaders(self):
-        return zip(*[DataLoader(dataset=dataset, num_workers=1, batch_size=None) for dataset in self.datasets])
+        return zip(*[DataLoader(dataset=dataset, num_workers=1, batch_size=2) for dataset in self.datasets])
 
     def __iter__(self):
         for batch_parts in self.get_stream_loaders():
