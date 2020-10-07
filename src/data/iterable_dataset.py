c0ebba070af4b82d1bab382ce3052be2792c807a
Adam Cohn <38065591+adcoh@users.noreply.github.com>
2020-10-07 12:51:48 +0300

    Streamlined subtrack splitting

--- src/data/iterable_dataset.py
+++ src/data/iterable_dataset.py
@@ -330,79 +330,90 @@ class DataDict(object):
                       for col in columns_to_check]
         df['usable'] = np.select(conditions, conditions, default=False)
         df.loc[df['is_validation'], 'usable'] = False
-        df.loc[df['is_validation'].shift(1).bfill(), 'usable'] = False
+        df.loc[df['is_validation'].shift(1).fillna(False), 'usable'] = False
+        df['usable'] = ~df['usable']
         # save validation segments to object and drop from current DF
-        val_df = df.loc[df.is_validation].copy().set_index('segment_id')
+        val_df = df.loc[df.is_validation].copy().set_index(['track_id', 'segment_id'])
         df = df.loc[~df.is_validation].copy()
         # TODO find a better way to create subtracks already here - perhaps using a smart groupby transform to group only consecutive segment ids
         # df['subtrack_id'] = df.groupby('track_id')['segment_id'].transform('rank')
         # df.groupby((df['usable'].shift() != df['usable']).cumsum())
-        df_tracks = df.groupby('track_id').agg(target_type=pd.NamedAgg(column="target_type", aggfunc=list),
-                                               usable=pd.NamedAgg(column="usable", aggfunc=list),
-                                               output_array=pd.NamedAgg(column="iq_sweep_burst", aggfunc=list),
-                                               doppler_burst=pd.NamedAgg(column="doppler_burst", aggfunc=list),
-                                               segment_id=pd.NamedAgg(column="segment_id", aggfunc=list),
-                                               )
-        df_tracks['output_array'] = df_tracks['output_array'].apply(lambda x: np.concatenate(x, axis=-1))
+        df['subtrack_id'] = df.groupby('track_id').usable.cumsum()
+        df_tracks = df.groupby(['track_id', 'subtrack_id']).agg(target_type=pd.NamedAgg(column="target_type", aggfunc=list),
+                                                                usable=pd.NamedAgg(column="usable", aggfunc=list),
+                                                                output_array=pd.NamedAgg(column="iq_sweep_burst", aggfunc=list),
+                                                                doppler_burst=pd.NamedAgg(column="doppler_burst", aggfunc=list),
+                                                                )
+        val_tracks = val_df.groupby(['track_id', 'segment_id']).agg(target_type=pd.NamedAgg(column="target_type", aggfunc=list),
+                                                                    usable=pd.NamedAgg(column="usable", aggfunc=list),
+                                                                    output_array=pd.NamedAgg(column="iq_sweep_burst", aggfunc=list),
+                                                                    doppler_burst=pd.NamedAgg(column="doppler_burst", aggfunc=list),
+                                                                    )
+        df_tracks['output_array'] = df_tracks['output_array'].apply(lambda x: np.concatenate(x, axis=1))
+        val_tracks['output_array'] = val_tracks['output_array'].apply(lambda x: np.concatenate(x, axis=1))
 
         if self.output_data_type == 'scalogram':
             print('Converting IQ matricies to Scalogram')
             df_tracks['output_array'] = df_tracks['output_array'].progress_apply(iq_to_scalogram,
                                                                                  transformation=self.config['mother_wavelet'],
                                                                                  scale=self.config['wavelet_scale'])
-            val_df['output_array'] = val_df['iq_sweep_burst'].progress_apply(iq_to_scalogram,
-                                                                             self.config['mother_wavelet'],
-                                                                             self.config['wavelet_scale'])
+            val_tracks['output_array'] = val_tracks['iq_sweep_burst'].progress_apply(iq_to_scalogram,
+                                                                                     transformation=self.config['mother_wavelet'],
+                                                                                     scale=self.config['wavelet_scale'])
         else:
             print('Converting IQ matricies to Spectrogram')
             df_tracks['output_array'] = df_tracks['output_array'].progress_apply(iq_to_spectogram)
-            val_df['output_array'] = val_df['iq_sweep_burst'].progress_apply(iq_to_spectogram)
+            val_tracks['output_array'] = val_tracks['output_array'].progress_apply(iq_to_spectogram)
 
         df_tracks['doppler_burst'] = df_tracks['doppler_burst'].apply(lambda x: np.concatenate(x, axis=-1))
+        val_tracks['doppler_burst'] = val_tracks['doppler_burst'].apply(lambda x: np.concatenate(x, axis=-1))
         df_tracks['target_type'] = df_tracks['target_type'].apply(np.array)
+        val_tracks['target_type'] = val_tracks['target_type'].apply(np.array)
         df_tracks['usable'] = df_tracks['usable'].apply(np.array)
-        df_tracks['segment_id'] = df_tracks['segment_id'].apply(np.array)
-        return df_tracks.to_dict(orient='index'), val_df.to_dict(orient='index')
+        val_tracks['usable'] = val_tracks['usable'].apply(np.array)
+        train_segments = [_Segment(segment_id=f'{k[0]}_{k[1]}', **v) for k, v in df_tracks.to_dict(orient='index').items()]
+        val_segments = [_Segment(segment_id=f'{k[0]}_{k[1]}', **v) for k, v in val_tracks.to_dict(orient='index').items()]
+        return train_segments, val_segments
 
 
-def filter_usable_segments(data, output_data_type) -> List[_Segment]:
-    """This algorithm works on the assumption that we have a Boolean Array in data['usable'] indicating whether a
-    given segment is to be used for augmentation -- eligibility is set in get_track_level_data.
-    It will create a list of the longest adjacent sub-tracks contained in a track and return the broken-up track as a
-    list of lists, along with the corresponding doppler_burst and label arrays as lists of lists in a dictionary.
-
-    Arguments:
-            data -- {dict} -- data for one track with parameters: {'output_array', 'doppler_burst',
-                                                                    'target_type', 'usable'}
-    """
-    track = data['output_array']
-    burst = data['doppler_burst']
-    previous_i = 0
-    tracks = []
-    # TODO remove this, the splitting algorithm isn't working as it should after removing the validation segments in line 336, replace with operation on dataframe to create subtrack/segment IDs already at that stage
-    for i, use in enumerate(data['usable']):
-        if not use:
-            if data['usable'][previous_i]:
-                if i - previous_i > 0:
-                    start = previous_i * 32
-                    end = i * 32
-                    if output_data_type == 'scalogram':
-                        output_array = track[:, start:end, :]
-                    else:
-                        output_array = track[:, start:end]
-
-                    tracks.append(_Segment(segment_id=i,
-                                           output_array=output_array,
-                                           doppler_burst=burst[start:end],
-                                           target_type=data['target_type'][i]))
-            previous_i = i + 1
-    if not tracks:
-        if track.shape[1] == 32:
-            tracks.append(_Segment(segment_id=data['segment_id'][0],
-                                   output_array=track,
-                                   doppler_burst=burst,
-                                   target_type=data['target_type']))
-    return tracks
+# def filter_usable_segments(data, output_data_type) -> List[_Segment]:
+#     """This algorithm works on the assumption that we have a Boolean Array in data['usable'] indicating whether a
+#     given segment is to be used for augmentation -- eligibility is set in get_track_level_data.
+#     It will create a list of the longest adjacent sub-tracks contained in a track and return the broken-up track as a
+#     list of lists, along with the corresponding doppler_burst and label arrays as lists of lists in a dictionary.
+#
+#     Arguments:
+#             data -- {dict} -- data for one track with parameters: {'output_array', 'doppler_burst',
+#                                                                     'target_type', 'usable'}
+#     """
+#     track = data['output_array']
+#     burst = data['doppler_burst']
+#     previous_i = 0
+#     tracks = []
+#     # TODO remove this, the splitting algorithm isn't working as it should after removing the validation segments in line 336, replace with operation on dataframe to create subtrack/segment IDs already at that stage
+#     for i, use in enumerate(data['usable']):
+#         if not use:
+#             if data['usable'][previous_i]:
+#                 if i - previous_i > 0:
+#                     start = previous_i * 32
+#                     end = i * 32
+#                     if output_data_type == 'scalogram':
+#                         output_array = track[:, start:end, :]
+#                     else:
+#                         output_array = track[:, start:end]
+#
+#                     tracks.append(_Segment(segment_id=i,
+#                                            output_array=output_array,
+#                                            doppler_burst=burst[start:end],
+#                                            target_type=data['target_type'][i]))
+#             previous_i = i + 1
+#     if not tracks:
+#         if track.shape[1] == 32:
+#             tracks.append(_Segment(segment_id=data['segment_id'][0],
+#                                    output_array=track,
+#                                    doppler_burst=burst,
+#                                    target_type=data['target_type']))
+#     return tracks
 
 
 class StreamingDataset(IterableDataset):
@@ -432,7 +443,7 @@ class StreamingDataset(IterableDataset):
         self.is_val = is_val
         random.shuffle(self.random_index)
 
-    def segments_generator(self, data_dict) -> List[_Segment]:
+    def segments_generator(self, segment_list: List[_Segment]) -> List[_Segment]:
         """
         Generates new and/or augmented segments according to configuration parameters.
         Returns a dictionary containing the merged set of segments indexed by
@@ -448,22 +459,20 @@ class StreamingDataset(IterableDataset):
                      get_vertical_flip -- {bool} -- Flag to add vertical flips
                      block_size -- {int} -- Max number of samples allowed to be held in a memory
         """
-        filtered_data = filter_usable_segments(data=data_dict, output_data_type=self.config['output_data_type'])
-
         if self.config.get('get_shifts'):
-            resplit_data = create_new_segments_from_splits(filtered_data, nsplits=1)
+            segment_list = create_new_segments_from_splits(segment_list, nsplits=1)
         else:
-            resplit_data = create_new_segments_from_splits(filtered_data, nsplits=32)
+            segment_list = create_new_segments_from_splits(segment_list, nsplits=32)
 
         if self.config.get('get_vertical_flip'):
-            flips = create_flipped_segments(resplit_data, flip_type='vertical')
-            resplit_data = resplit_data + flips
+            flips = create_flipped_segments(segment_list, flip_type='vertical')
+            segment_list = segment_list + flips
         if self.config.get('get_horizontal_flip'):
-            flips = create_flipped_segments(resplit_data, flip_type='horizontal')
-            resplit_data = resplit_data + flips
+            flips = create_flipped_segments(segment_list, flip_type='horizontal')
+            segment_list = segment_list + flips
 
-        # return DataLoader(resplit_data, batch_size=1, shuffle=True)
-        return resplit_data
+        # return DataLoader(segment_list, batch_size=1, shuffle=True)
+        return segment_list
 
     def get_segment_stream(self):
 
