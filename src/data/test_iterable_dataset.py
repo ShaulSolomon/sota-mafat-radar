b319e6a0439954d622feda5e0ebdcd0b879106d7
Adam Cohn <acohn74@gmail.com>
2020-10-13 22:27:28 +0300

    Scalograms test

--- src/data/test_iterable_dataset.py
+++ src/data/test_iterable_dataset.py
@@ -27,9 +27,9 @@ for creds_path in creds_path_ar:
 sys.path.insert(0, os.path.join(PATH_ROOT))
 from src.data.iterable_dataset import Config, DataDict, StreamingDataset, MultiStreamDataLoader
 
-config = Config(file_path=PATH_DATA, num_tracks=3, valratio=6, get_shifts=True, output_data_type='spectrogram',
+config = Config(file_path=PATH_DATA, num_tracks=3, valratio=6, get_shifts=True, output_data_type='scalogram',
                 get_horizontal_flip=True, get_vertical_flip=True, mother_wavelet='cgau1', wavelet_scale=3,
-                batch_size=50, tracks_in_memory=2, include_doppler=True, shift_segment=16, shuffle_stream=True)
+                batch_size=50, tracks_in_memory=25, include_doppler=True, shift_segment=8, shuffle_stream=True)
 
 dataset = DataDict(config=config)
 track_count = len(dataset.train_data) + len(dataset.val_data)
